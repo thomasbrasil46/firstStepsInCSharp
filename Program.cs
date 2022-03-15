09064d480a78265259f05e8712fa8d72c8ba3e07
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-03-14 21:49:13 -0300

    First steps in C# language.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//importacoes
+using System;
+
+//divisoes logicas
+namespace MeuApp
+{
+    //programa
+    class Program
+    {
+                    //metodo
+        static void Main(string[] args)
+        {
+            var texto = "Testando";
+            Console.WriteLine(texto);
+        }
+    }
+}
