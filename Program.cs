3de47a103f6c5a4f4f013d7e04d2735576f0b98c
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-03-15 15:26:18 -0300

    Walkthrough explicit and implicit conversions, alias, parse, convert and operators.

--- Program.cs
+++ Program.cs
@@ -10,8 +10,18 @@ namespace MeuApp
                     //metodo
         static void Main(string[] args)
         {
-            var texto = "Testando";
-            Console.WriteLine(texto);
+            int inteiro = 100;
+            float real = 25.8f;
+
+            //real = inteiro;
+            //inteiro = (int)real;
+            //string valorReal = real.ToString();
+            //inteiro = int.Parse(valorReal);
+            inteiro = Convert.ToInt32(real);
+
+            ///Console.WriteLine(valorReal);
+            Console.WriteLine(inteiro);
+            Console.WriteLine(Convert.ToBoolean(1));
         }
     }
 }
