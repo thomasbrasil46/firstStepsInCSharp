2e7552ef2e62f910e2b04173854a122ee23a6a21
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-03-15 17:01:45 -0300

    Walkthrough Conditional Structures: Switch.

--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
-﻿//importacoes
-using System;
+﻿using System;
 
-//divisoes logicas
 namespace MeuApp
 {
-    //programa
     class Program
     {
-                    //metodo
         static void Main(string[] args)
         {
-            int inteiro = 100;
-            float real = 25.8f;
-
-            //real = inteiro;
-            //inteiro = (int)real;
-            //string valorReal = real.ToString();
-            //inteiro = int.Parse(valorReal);
-            inteiro = Convert.ToInt32(real);
-
-            ///Console.WriteLine(valorReal);
-            Console.WriteLine(inteiro);
-            Console.WriteLine(Convert.ToBoolean(1));
+            string valor = "andre";
+            switch (valor)
+            {
+                case "joao": Console.WriteLine("Não é o cara"); break;
+                case "marcelo": Console.WriteLine("Não é o cara"); break;
+                case "andre": Console.WriteLine("É o cara"); break;
+                default: Console.WriteLine("Não encontrei"); break;
+            }
         }
     }
 }
